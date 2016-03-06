fbdc109799d7e08fda8d9129e217f582f715339e
Matthias <matthias@toxicode.fr>
2016-03-06 16:28:31 +0700

    better seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,16 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 words_list = [
-  { definition_fr: "météo", definition_en: "weather", definition_kh: "", phonetic: "akasotièt", word_type: "", tag_list: "" },
-  { definition_fr: "pomme", definition_en: "apple", definition_kh: "", phonetic: "pom", word_type: "", tag_list: "" },
-  { definition_fr: "voiture", definition_en: "car", definition_kh: "", phonetic: "", word_type: "", tag_list: "transport" },
-  { definition_fr: "vélo", definition_en: "bike", definition_kh: "", phonetic: "cang", word_type: "", tag_list: "transport" },
-  { definition_fr: "moto", definition_en: "motorbike", definition_kh: "", phonetic: "moto", word_type: "", tag_list: "transport" },
-  { definition_fr: "salut", definition_en: "hi", definition_kh: "", phonetic: "susdey", word_type: "", tag_list: "" },
-  { definition_fr: "merci", definition_en: "thank you", definition_kh: "", phonetic: "orkon", word_type: "", tag_list: "" },
-  { definition_fr: "aimer", definition_en: "to love", definition_kh: "", phonetic: "srolagne", word_type: "", tag_list: "" },
-  { definition_fr: "petit", definition_en: "small", definition_kh: "", phonetic: "touy", word_type: "", tag_list: "" },
-  { definition_fr: "grand", definition_en: "big", definition_kh: "", phonetic: "throm", word_type: "", tag_list: "" }
+  { definition_fr: "météo", definition_en: "weather", definition_kh: "អាកាសធាតុ", definition_ph: "akasotièt", word_type: "noun", fr_tag_list: "Conversation", en_tag_list: "Conversation" },
+  { definition_fr: "pomme", definition_en: "apple", definition_kh: "ផ្លែប៉ោម", definition_ph: "pom", word_type: "noun", fr_tag_list: "Fruits et légumes", en_tag_list: "Fruits and vegetables" },
+  { definition_fr: "voiture", definition_en: "car", definition_kh: "ឡាន", definition_ph: "lan", word_type: "noun", fr_tag_list: "Transport", en_tag_list: "Transport" },
+  { definition_fr: "vélo", definition_en: "bike", definition_kh: "កង់", definition_ph: "cang", word_type: "noun", fr_tag_list: "Transport", en_tag_list: "Transport" },
+  { definition_fr: "moto", definition_en: "motorbike", definition_kh: "ម៉ូតូ", definition_ph: "moto", word_type: "noun", fr_tag_list: "Transport", en_tag_list: "Transport" },
+  { definition_fr: "salut", definition_en: "hi", definition_kh: "សួស្តី", definition_ph: "susdey", word_type: "noun", fr_tag_list: "Conversation, Présentation", en_tag_list: "Conversation, Introduction" },
+  { definition_fr: "merci", definition_en: "thank you", definition_kh: "អរគុណ", definition_ph: "orkon", word_type: "noun", fr_tag_list: "Conversation, Présentation", en_tag_list: "Conversation, Introduction" },
+  { definition_fr: "aimer", definition_en: "to love", definition_kh: "ស្រឡាញ់", definition_ph: "srolagne", word_type: "verb", fr_tag_list: "Conversation", en_tag_list: "Conversation" },
+  { definition_fr: "petit", definition_en: "small", definition_kh: "តូច", definition_ph: "touy", word_type: "adjective", fr_tag_list: "Description", en_tag_list: "Description" },
+  { definition_fr: "grand", definition_en: "big", definition_kh: "ធំ", definition_ph: "throm", word_type: "adjective", fr_tag_list: "Description", en_tag_list: "Description" }
 ]
 
 words_list.each do |word|
@@ -24,7 +24,9 @@ words_list.each do |word|
     definition_fr: word[:definition_fr],
     definition_en: word[:definition_en],
     definition_kh: word[:definition_kh],
-    phonetic: word[:phonetic],
+    definition_ph: word[:definition_ph],
     word_type: word[:word_type],
-    tag_list: word[:word_type])
+    fr_tag_list: word[:fr_tag_list],
+    en_tag_list: word[:en_tag_list]
+  )
 end
